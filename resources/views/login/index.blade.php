b07c2437059476691c3c1289e8e722a4f8557f85
MuhammadAsharul <masharul51@gmail.com>
2022-06-26 08:54:09 +0700

    memperbaiki halaman login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,42 +1,38 @@
 @extends('landingpage.index')
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-lg-4 rounded float-start">
-      @if(session()->has('success'))
-      <div class="alert alert-success">
-        <p>{{ session('success') }}</p>
-    </div>
-      @endif
-
-      @if(session()->has('login_error'))
-      <div class="alert alert-danger">
-        <p>{{ session('login_error') }}</p>
-    </div>
-      @endif
-        <main class="form-signin w-100 m-auto">
-            <small class="d-block text-right mt-3">Don't have an account? <a href="/register"> Sign Up</a></small>
-            <h1 class="h3 mb-3 fw-normal text-left">Sign In</h1>
-            <form method="POST" action="{{url('/auth')}}">
-              @csrf
-                <div class="text-center"><button>
-                    <a href="#" class="btn btn-ligh">
-                        <i class="fa fa-google"></i>Continue with google</a> </button>  <br/>
-               </div>
-              <div class="form-floating">
-                <label for="username">Username</label>
-                <input type="text" class="form-control" id="username" name="username" placeholder="username">
-              </div>
-              <div class="form-floating">
-                  <label for="password">Password</label>
-                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
-                <small class="d-block text-right"><a href="#"> Forget your password?</a></small>
-              </div>
-              <button class="btn btn-lg btn-primary d-block btn-center" type="submit">Login</button> <br/>
-            </form>
-        </main>
-    </div>
-    <div class="card" style="width: 18rem;">
-        <img src="{{ asset('img/projek/l2.jpg') }}" class="card-img-top" alt="...">
+    <div class="container d-flex align-items-center border border-1">    
+      {{-- form gambar--}}
+      <div class="d-flex align-items-center">
+      <div class="col-lg-6">
+        <small class="d-flex mb-5 justify-content-end">Don't have an account ? <a href="/register" class="text-purple">&nbsp Sign Up</a></small>
+        <h4 class="font-weight-bold mb-4">Sign In</h4>
+        
+          <form method="POST" action="{{url('/auth')}}">
+            @csrf
+              <div class="text-center ">
+                <button class="w-100 rounded bg-light">
+                      <a href="#" class="btn btn-light font-weight-medium">
+                      <i class="fa fa-google"></i>Continue with google</a> 
+                </button>
+             </div>
+            <div>
+              <label for="username">Username</label>
+              <input type="text" class="form-control" id="username" name="username" placeholder="username">
+            </div>
+            <div>
+                <label for="password">Password</label>
+              <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+              <small class="d-block text-right "><a href="#" class="text-purple"> Forget your password?</a></small>
+            </div>
+            <div class="d-flex justify-content-center">
+            <button class="btn btn-primary w-50 mt-4" type="submit">Login</button>
+          </div>
+          </form>
+        </div>
+      <div class="col-lg-6">
+        <img src="{{ asset('img/projek/login.jpg') }}" class="img-fluid" alt="...">
       </div>
-</div>
+      {{-- akhir form gambar --}}
+    </div>
+    </div>
 @endsection
