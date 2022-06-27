dd3236daa3a80424dc41f3ae68eeda69690ae479
tripng <triwahyu.png@gmail.com>
2022-06-27 12:21:24 +0700

    Update detail.blade.php

--- resources/views/landingpage/detail.blade.php
+++ resources/views/landingpage/detail.blade.php
@@ -25,12 +25,12 @@
                                 <h3>Rp{{number_format($barang->harga,0,',','.')}} <span>70.00</span></h3>
                             </div>
                             <div class="product__details__cart__option">
-                                <div class="quantity">
+                                {{-- <div class="quantity">
                                     <div class="pro-qty">
                                         <input type="text" value="1">
                                     </div>
-                                </div>
-                                <a href="#" class="primary-btn">add to cart</a>
+                                </div> --}}
+                                {{-- <a href="#" class="primary-btn">add to cart</a> --}}
                             </div>
                             <div class="product__details__last__option">
                                 <ul>
@@ -210,74 +210,46 @@
             <div class="row">
                 <div class="col-lg-3 col-md-6 col-sm-6 col-sm-6">
                     <div class="product__item">
-                        <a href=""><div class="product__item__pic set-bg" data-setbg="img/product/product-1.jpg">
+                        <a href=""><div class="product__item__pic set-bg" data-setbg="{{asset('img/product/product-1.jpg')}}">
                             <span class="label">New</span>
                         </div></a>
                         <div class="product__item__text">
                             <h6>Piqué Biker Jacket</h6>
                             <a href="#" class="add-cart">+ Add To Cart</a>
-                            <div class="rating">
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                            </div>
                             <h5>$67.24</h5>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6 col-sm-6">
                     <div class="product__item">
-                        <a href=""><div class="product__item__pic set-bg" data-setbg="img/product/product-2.jpg">
+                        <a href=""><div class="product__item__pic set-bg" data-setbg="{{asset('img/product/product-2.jpg')}}">
                         </div></a>
                         <div class="product__item__text">
                             <h6>Piqué Biker Jacket</h6>
                             <a href="#" class="add-cart">+ Add To Cart</a>
-                            <div class="rating">
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                            </div>
                             <h5>$67.24</h5>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6 col-sm-6">
                     <div class="product__item sale">
-                        <a href=""><div class="product__item__pic set-bg" data-setbg="img/product/product-3.jpg">
+                        <a href=""><div class="product__item__pic set-bg" data-setbg="{{asset('img/product/product-3.jpg')}}">
                             <span class="label">Sale</span>
                         </div></a>
                         <div class="product__item__text">
                             <h6>Multi-pocket Chest Bag</h6>
                             <a href="#" class="add-cart">+ Add To Cart</a>
-                            <div class="rating">
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                            </div>
                             <h5>$43.48</h5>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6 col-sm-6">
                     <div class="product__item">
-                        <a href=""><div class="product__item__pic set-bg" data-setbg="img/product/product-4.jpg">
+                        <a href=""><div class="product__item__pic set-bg" data-setbg="{{asset('img/product/product-4.jpg')}}">
                         </div></a>
                         <div class="product__item__text">
                             <h6>Diagonal Textured Cap</h6>
                             <a href="#" class="add-cart">+ Add To Cart</a>
-                            <div class="rating">
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                                <i class="fa fa-star-o"></i>
-                            </div>
                             <h5>$60.9</h5>
                         </div>
                     </div>
